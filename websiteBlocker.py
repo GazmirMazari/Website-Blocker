fff6ae110f48dc8eda704fa3f84ecafee6a270d6
Gazmir Mazari <37550822+GazmirDi@users.noreply.github.com>
2020-03-16 19:18:00 -0400

    First phase

---
+++ websiteBlocker.py
@@ -0,0 +1,10 @@
+import time
+host_path = "C:\Windows\System32\drivers\etc\hosts"
+redirect = "127.0.0.1"
+
+website_list = ["www.facebook.com","facebook.com", "www.instagram.com", "instagram.com"]
+
+while True:
+
+    print(1)
+    time.sleep(5)
