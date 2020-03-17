87deacef22aea03672e61ba2e211997adfc40c91
Gazmir Mazari <gmazari@serviceexpress.com>
2020-03-16 20:40:59 -0400

    Done

--- websiteBlocker.py
+++ websiteBlocker.py
@@ -1,14 +1,29 @@
-from datetime import datetime, timedelta, time
-from pytz import timezone
+from datetime import datetime as dt
+import time
 
-est = pytz.ut
-
+host_temp ="hosts"
 host_path = "C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
 
-website_list = ["www.facebook.com","facebook.com", "www.instagram.com", "instagram.com"]
+websites= ["www.facebook.com","facebook.com", "www.instagram.com", "instagram.com"]
 
 while True:
-    if x 
-    print(1)
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,21):
+        print ("Working hours...")
+        with open(host_path,'r+') as file:
+            content=file.read()
+            for site in websites:
+                if site in content:
+                    pass
+                else:
+                    file.write(redirect+" "+site+"\n")
+    else: 
+         with open(host_path,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(site in line for site in websites):
+                    file.write(line)
+            file.truncate()
+    print("Fun hours...")
     time.sleep(5)
